a41d539bcf2a1fe40f53456178b870d7a1d8ef36
camunozn <camunozn89@gmail.com>
2022-10-07 17:56:16 -0500

    added geospatial queriesand aggregation

--- models/tourModel.js
+++ models/tourModel.js
@@ -34,6 +34,7 @@ const tourSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, 'Rating must be above 1.0'],
       max: [5, 'Rating must be below 5.0'],
+      set: val => Math.round(val * 100) / 100,
     },
     ratingsQuantity: {
       type: Number,
@@ -115,6 +116,7 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.index({ price: 1, ratingsAverage: -1 });
 tourSchema.index({ slug: 1 });
+tourSchema.index({ startLocation: '2dsphere' });
 
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
@@ -157,10 +159,10 @@ tourSchema.pre(/^find/, function (next) {
 
 ////////////////////////////////////////////////////////
 // AGGREGATION MIDDLEWARE
-tourSchema.pre('aggregate', function (next) {
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-  next();
-});
+// tourSchema.pre('aggregate', function (next) {
+//   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+//   next();
+// });
 
 ////////////////////////////////////////////////////////
 const Tour = mongoose.model('Tour', tourSchema);
