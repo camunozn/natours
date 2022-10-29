e217364f014756c183b677cc9dac3585b28fbf0b
camunozn <camunozn89@gmail.com>
2022-10-29 17:27:35 -0500

    fixed booking message bug

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -6,11 +6,9 @@ const AppError = require('../utils/appError');
 
 exports.alerts = (req, res, next) => {
   const { alert } = req.query;
-
-  if (alert === 'bookings')
+  if (alert === 'booking')
     res.locals.alert =
       'Your booking was successful! Please check your email for a confirmation. If your booking does not show immediatly, please come back later.';
-
   next();
 };
 
