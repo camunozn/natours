47b3d3d3c500788cb8ba27227e417d0f7b055447
camunozn <camunozn89@gmail.com>
2022-10-28 23:38:23 -0500

    prepared for deployment

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 const app = express();
 
@@ -126,10 +127,12 @@ app.use(
   })
 );
 
+// Compress text
+app.use(compression());
+
 // Test middleware to calculate request time
 // app.use((req, res, next) => {
 //   req.requestTime = new Date().toISOString();
-//   console.log(req.headers)
 //   next();
 // });
 
